b2f07a9674dc7c17ccd3c95cb7ebf7559a5acbd8
mta630 <mta630@ddubb.net>
2024-07-29 11:15:28 -0600

    gitignore and some pandas work for day 1

--- pandas/day 1/pandas.py
+++ pandas/day 1/pandas.py
@@ -1,1 +1,19 @@
 import pandas as pd
+
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    return world[(world['area'] >= 3000000)]
+
+
+# Sample data
+data = {
+    'name': ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola'],
+    'continent': ['Asia', 'Europe', 'Africa', 'Europe', 'Africa'],
+    'area': [652230, 28748, 2381741, 468, 1246700],
+    'population': [25500100, 2831741, 37100000, 78115, 20609294],
+    'gdp': [20343000000, 12960000000, 188681000000, 3712000000, 100990000000]
+}
+
+result = big_countries(pd.DataFrame(data))
+
+print(result)
