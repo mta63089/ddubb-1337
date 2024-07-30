ba9138aa1d3e0ce39d2e84199ccc877c6715e6f3
mta630 <mta630@ddubb.net>
2024-07-30 11:22:40 -0600

    day 2 of panda challenged, number 1757 problem finished

---
+++ pandas/1757/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    fat=products["low_fats"] == 'Y'
+    rec=products["recyclable"] == 'Y'
+    fin=products[fat & rec]
+    return fin[["product_id"]]
+    
+    
+def find_products_alt(products: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame(products[(products.low_fats == 'Y') & (products.recyclable == 'Y')]['product_id'])
